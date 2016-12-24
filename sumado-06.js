c366e9fc1ce5a67270dace4dabc58b0b59ae5625
Willie Verger <willie.mensa@gmail.com>
2016-12-24 19:30:05 -0300

    Drag and Drop + Suma de poligonos
    Después de resolver el problema de Drag and Drop se agregó posicionar la
    suma de los poligonos.
    Próximo paso será preparar el vector de vértices donde se alojaran los
    números

--- sumado-06.js
+++ sumado-06.js
@@ -1,19 +1,7 @@
 //	sumado.js
 //	codigo para plantear un sumado. Por ahora de 3 x 3
 //	Tarea actual
-//		Preparar vector con datos de vértices
-//			valor objetivo
-//			valor asignado o vacio
-//			Tener en cuenta asociar las posiciones en que se deja el 
-//			sprite-numero con los vertices para posibilitar analisis.
-
-//		Preparar vector con numeros a asignar (sprites)
-//			valor numerico
-//		Preparar vector para estacionamiento de numeros a asignar
-//			id cochera
-//			valor ocupado o vacio
-
-//	1	Tomar / generar la data de poligonos y vértices: (linea 150)
+//	1	Preparación de un vector con la data de los vértices: (linea 126)
 //		diagrama con la correspondecia de vértices a su id y
 //		poligonos a su ID
 
@@ -22,9 +10,6 @@
 //			http://skielcast.pythonanywhere.com/3x3_v1
 //		{"vertexes": [1, 6, 9, 8, 7, 4, 5, 2, 3], "poligons": [14, 16, 26, 22, 14, 12]}
 //		{"vertexes": [2, 4, 9, 6, 8, 5, 3, 7, 1], "poligons": [14, 16, 26, 24, 14, 16]}
-
-//		Falta cargar los vectores aSumaPolig y aPosPolig 
-//		que contienen los valores de los poligonos a presentar y la posición de los textos
 
 //	3	agregar menu inicial
 //	4	contador de tiempo, avisos de fin del entretenimiento, tabla de solucionadores y reiniciar
@@ -48,8 +33,7 @@ var t = undefined,
     pointer = undefined,
     grid = undefined,
     draggableObjects = undefined,
-	aVertices = [],				//	array con datos de vertices
-	DEBUG = true;
+	DEBUG = false;
 
 
 	//load a JSON file and run the `setup` function when it's done 
@@ -64,12 +48,19 @@ function setup() {
 	//	$.getJSON('http://skielcast.pythonanywhere.com/2x3_v1', function(data) {	});
 
 	//	Set the canvas's border style and background color
-	renderer.view.style.border = "4px solid red";
+	renderer.view.style.border = "4px dashed black";
 	renderer.backgroundColor = "0xeeeeee";
 
+	//Define any variables that are used in more than one function
+	//////////////////////////////////////////
+	//	aqui comienza el uso de librería tink
+	//	t = new Tink(PIXI, renderer.view);
+
+	//	pointer = t.makePointer();
+
+
 	//Define variables that might be used in more than one function
-	var tablero, i,	aSumaPolig, aPosPolig ;
-	var message 
+	var tablero
 
 	//	pruebo armar un array con los numeros
 	var num, cImagen, aNumeros = [];
@@ -155,19 +146,33 @@ function setup() {
 	//		id del vértice (codificado adecuadamente para identificarlo facil)
 	//		valor objetivo
 	//		id.num colocado: codigo del número colocado en el vértice o señal de estar vacio
-	//
+
 
 	////////////////////////////////////////////////////
 	//	colocamos las sumas de los poligonos en posición
-	aSumaPolig = [14, 16, 26, 22, 14, 12];
-	aPosPolig = [[210, 140],[140, 205],[370, 170],[170, 370],[340, 405],[410, 340]];
+	var message = new PIXI.Text( "A", { font: "56px Calibri", fill : "green", align : 'right'});
+	message.position.set(210, 140);
+	stage.addChild(message);
 
-	for ( i = 0; i < 6; i++)
-	{
-		message = new PIXI.Text( aSumaPolig[i], { font: "56px Calibri", fill : "green", align : 'right'});
-		message.position.set(aPosPolig [i][0], aPosPolig [i][1]);
-		stage.addChild(message);
-	}
+	message = new PIXI.Text(	"B", {font: "56px Calibri", fill : "green", align : 'right'});
+	message.position.set(140, 205);
+	stage.addChild(message);
+
+	message =	new PIXI.Text(	"C", {font: "56px Calibri", fill : "green", align : 'right'});
+	message.position.set(370, 170);
+	stage.addChild(message);
+
+	message = new PIXI.Text(	"D", {font: "56px Calibri", fill : 'green', align : 'center'});
+	message.position.set(170, 370);
+	stage.addChild(message);
+
+	message = new PIXI.Text(	"F", {font: "56px Calibri", fill : 'green', align : 'center'});
+	message.position.set(340, 405);
+	stage.addChild(message);
+
+	message = new PIXI.Text(	"E", {font: "56px Calibri", fill : 'green', align : 'center'});
+	message.position.set(410, 340);
+	stage.addChild(message);
 
 	//////////////////////////
 	//	Una grilla para ubicarnos en el canvas
@@ -224,7 +229,8 @@ function onDragEnd()
 {
 	var gridstep, nLineaOffset = 100;
 
-	//	this sería el sprite con numero a posicionar
+	//	console.log( "ingresando a DragMove: " + this.position.x + "," + this.position.y );
+
     if (this.dragging)
     {
         var newPosition = this.data.getLocalPosition(this.parent);
@@ -234,10 +240,10 @@ function onDragEnd()
 		gridstep = grid[0];
 		console.log( "newPosition.x : " + newPosition.x );
 		console.log( "Math.round(newPosition.x / gridstep): " + Math.round(newPosition.x / gridstep) );
-		console.log( "(newPosition.x / gridstep): " + (newPosition.x / gridstep) );
-		newPosition.x = nLineaOffset + ( Math.round(( newPosition.x - nLineaOffset )/ gridstep ) * gridstep );
 
-		gridstep = grid[1];											
+		newPosition.x = nLineaOffset + ( Math.round(( newPosition.x - nLineaOffset )/ gridstep ) * gridstep );
+		gridstep = grid[1];
+											
 		newPosition.y = nLineaOffset + ( Math.round((newPosition.y - nLineaOffset )/ gridstep ) * gridstep );
 		//	lo que sigue no lo uso por ahora 
 		//		if(containment && !(x > 0 || x2 < containment.width))
@@ -246,7 +252,8 @@ function onDragEnd()
 		//		}
 
 		console.log( "nLineaOffset + Math.round(newPosition.x / gridstep) * gridstep;" + newPosition.x )
-		console.log( "nLineaOffset + Math.round(newPosition.y / gridstep) * gridstep;" + newPosition.y )		
+		console.log( "nLineaOffset + Math.round(newPosition.y / gridstep) * gridstep;" + newPosition.y )
+		
 		console.log( "saliendo de ondragmove --> posicion: " + newPosition.x + ", " + newPosition.y );
 		/////////////////////////////////////////////////////
 
@@ -303,7 +310,4 @@ function animate() {
     // render the stage
     renderer.render(stage);
 }
-
-
-
 
--- sumado.js
+++ sumado.js
@@ -1,3 +1,35 @@
+//	sumado.js
+//	codigo para plantear un sumado. Por ahora de 3 x 3
+//	Tarea actual
+//		Preparar vector con datos de vértices
+//			valor objetivo
+//			valor asignado o vacio
+//			Tener en cuenta asociar las posiciones en que se deja el 
+//			sprite-numero con los vertices para posibilitar analisis.
+
+//		Preparar vector con numeros a asignar (sprites)
+//			valor numerico
+//		Preparar vector para estacionamiento de numeros a asignar
+//			id cochera
+//			valor ocupado o vacio
+
+//	1	Tomar / generar la data de poligonos y vértices: (linea 150)
+//		diagrama con la correspondecia de vértices a su id y
+//		poligonos a su ID
+
+//	2	decodificar data para el sumado actual, vienen en formato JSON 
+//		generada por
+//			http://skielcast.pythonanywhere.com/3x3_v1
+//		{"vertexes": [1, 6, 9, 8, 7, 4, 5, 2, 3], "poligons": [14, 16, 26, 22, 14, 12]}
+//		{"vertexes": [2, 4, 9, 6, 8, 5, 3, 7, 1], "poligons": [14, 16, 26, 24, 14, 16]}
+
+//		Falta cargar los vectores aSumaPolig y aPosPolig 
+//		que contienen los valores de los poligonos a presentar y la posición de los textos
+
+//	3	agregar menu inicial
+//	4	contador de tiempo, avisos de fin del entretenimiento, tabla de solucionadores y reiniciar
+//	5
+
 //Test that Pixi is working
 console.log(PIXI);
 
@@ -16,6 +48,7 @@ var t = undefined,
     pointer = undefined,
     grid = undefined,
     draggableObjects = undefined,
+	aVertices = [],				//	array con datos de vertices
 	DEBUG = true;
 
 
@@ -28,20 +61,15 @@ var t = undefined,
 
 function setup() {
 
-	//Set the canvas's border style and background color
-	renderer.view.style.border = "4px dashed black";
+	//	$.getJSON('http://skielcast.pythonanywhere.com/2x3_v1', function(data) {	});
+
+	//	Set the canvas's border style and background color
+	renderer.view.style.border = "4px solid red";
 	renderer.backgroundColor = "0xeeeeee";
 
-	//Define any variables that are used in more than one function
-	//////////////////////////////////////////
-	//	aqui comienza el uso de librería tink
-	//	t = new Tink(PIXI, renderer.view);
-
-	//	pointer = t.makePointer();
-
-
 	//Define variables that might be used in more than one function
-	var tablero
+	var tablero, i,	aSumaPolig, aPosPolig ;
+	var message 
 
 	//	pruebo armar un array con los numeros
 	var num, cImagen, aNumeros = [];
@@ -121,59 +149,53 @@ function setup() {
 
 	}
 
-	//	para hacer draggable los numeros
-	//	t.makeDraggable(aNumeros[0], aNumeros[1],aNumeros[2],aNumeros[3],aNumeros[4] );
+
+	////////////////////////////////////////////////////
+	//	Preparo un vector con la data de los vértices:
+	//		id del vértice (codificado adecuadamente para identificarlo facil)
+	//		valor objetivo
+	//		id.num colocado: codigo del número colocado en el vértice o señal de estar vacio
+	//
 
 	////////////////////////////////////////////////////
 	//	colocamos las sumas de los poligonos en posición
-	var message = new PIXI.Text( "A9", { font: "56px Calibri", fill : "green", align : 'right'});
-	message.position.set(210, 140);
-	stage.addChild(message);
+	aSumaPolig = [14, 16, 26, 22, 14, 12];
+	aPosPolig = [[210, 140],[140, 205],[370, 170],[170, 370],[340, 405],[410, 340]];
 
-	message = new PIXI.Text(	"B9", {font: "56px Calibri", fill : "green", align : 'right'});
-	message.position.set(140, 205);
-	stage.addChild(message);
-
-	message =	new PIXI.Text(	"C9", {font: "56px Calibri", fill : "green", align : 'right'});
-	message.position.set(370, 170);
-	stage.addChild(message);
-
-	message = new PIXI.Text(	"D9", {font: "56px Calibri", fill : 'green', align : 'center'});
-	message.position.set(170, 370);
-	stage.addChild(message);
-
-	message = new PIXI.Text(	"E9", {font: "56px Calibri", fill : 0x10ff10, align : 'center'});
-	message.position.set(340, 405);
-	stage.addChild(message);
-
-	message = new PIXI.Text(	"F9", {font: "56px Calibri", fill : 0x10ff10, align : 'center'});
-	message.position.set(410, 340);
-	stage.addChild(message);
+	for ( i = 0; i < 6; i++)
+	{
+		message = new PIXI.Text( aSumaPolig[i], { font: "56px Calibri", fill : "green", align : 'right'});
+		message.position.set(aPosPolig [i][0], aPosPolig [i][1]);
+		stage.addChild(message);
+	}
 
 	//////////////////////////
 	//	Una grilla para ubicarnos en el canvas
-	for (var i = 0; i < 12; i++)
-	{
-		//	lineas horizontales
-		var line = new PIXI.Graphics();
-		line.lineStyle(1, 0xff88ff, 1);
-		line.moveTo(0, 0);
-		line.lineTo(600, 00);
-		line.x = 0;
-		line.y = 50 * (i + 1);
-		stage.addChild(line);
-		//	console.log( "Linea en posicion: " + line.y );
+		if (DEBUG) 
+		{
+			for (var i = 0; i < 12; i++)
+			{
+				//	lineas horizontales
+				var line = new PIXI.Graphics();
+				line.lineStyle(1, 0xff88ff, 1);
+				line.moveTo(0, 0);
+				line.lineTo(600, 00);
+				line.x = 0;
+				line.y = 50 * (i + 1);
+				stage.addChild(line);
+				//	console.log( "Linea en posicion: " + line.y );
 
-		//	lineas verticales
-		var line = new PIXI.Graphics();
-		line.lineStyle(1, 0xff88ff, 1);
-		line.moveTo(0, 0);
-		line.lineTo(0, 600);
-		line.x = 50 * (i + 1);
-		line.y = 0;
-		stage.addChild(line);
+				//	lineas verticales
+				var line = new PIXI.Graphics();
+				line.lineStyle(1, 0xff88ff, 1);
+				line.moveTo(0, 0);
+				line.lineTo(0, 600);
+				line.x = 50 * (i + 1);
+				line.y = 0;
+				stage.addChild(line);
+			}
+		}
 
-	}
 
 	animate();
 
@@ -202,21 +224,22 @@ function onDragEnd()
 {
 	var gridstep, nLineaOffset = 100;
 
-	//	console.log( "ingresando a DragMove: " + this.position.x + "," + this.position.y );
-
+	//	this sería el sprite con numero a posicionar
     if (this.dragging)
     {
         var newPosition = this.data.getLocalPosition(this.parent);
+		//	parece que this.parent es el puntero del mouse
+
 		/////////////////////////////////////////////////////
 		//	aca modifico para tratar de introducir una grilla
 
 		gridstep = grid[0];
 		console.log( "newPosition.x : " + newPosition.x );
 		console.log( "Math.round(newPosition.x / gridstep): " + Math.round(newPosition.x / gridstep) );
-
+		console.log( "(newPosition.x / gridstep): " + (newPosition.x / gridstep) );
 		newPosition.x = nLineaOffset + ( Math.round(( newPosition.x - nLineaOffset )/ gridstep ) * gridstep );
-		gridstep = grid[1];
-											
+
+		gridstep = grid[1];											
 		newPosition.y = nLineaOffset + ( Math.round((newPosition.y - nLineaOffset )/ gridstep ) * gridstep );
 		//	lo que sigue no lo uso por ahora 
 		//		if(containment && !(x > 0 || x2 < containment.width))
@@ -225,8 +248,7 @@ function onDragEnd()
 		//		}
 
 		console.log( "nLineaOffset + Math.round(newPosition.x / gridstep) * gridstep;" + newPosition.x )
-		console.log( "nLineaOffset + Math.round(newPosition.y / gridstep) * gridstep;" + newPosition.y )
-		
+		console.log( "nLineaOffset + Math.round(newPosition.y / gridstep) * gridstep;" + newPosition.y )		
 		console.log( "saliendo de ondragmove --> posicion: " + newPosition.x + ", " + newPosition.y );
 		/////////////////////////////////////////////////////
 
@@ -283,4 +305,7 @@ function animate() {
     // render the stage
     renderer.render(stage);
 }
+
+
+
 
